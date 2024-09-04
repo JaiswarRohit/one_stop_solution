f67d339652fadb809318122c055b07819306988f
Rohit Jaiswar <rjrohit1306@gmail.com>
2024-09-04 16:10:02 +0530

    update

--- Pattern/Diamond_pattern.py
+++ Pattern/Diamond_pattern.py
@@ -1,3 +1,4 @@
+'''
 n = int(input("Enter your number: "))
 for a in range(n):
     for b in range(a, n):
@@ -15,3 +16,24 @@ for a in range(n):
     for d in range(a, n):
         print("*", end=" ")
     print()
+'''
+def Diamond_pattern(n):
+    for a in range(n-1):
+        for b in range(a, n):
+            print(" ", end=" ")
+        for c in range(a):
+            print("*", end=" ")
+        for d in range(a + 1):
+            print("*", end=" ")
+        print()
+    for a in range(n):
+        for b in range(a + 1):
+            print(" ", end=" ")
+        for c in range(a, n - 1):
+            print("*", end=" ")
+        for d in range(a, n):
+            print("*", end=" ")
+        print()
+
+a = int(input("Enter your number: "))
+Diamond_pattern(a)
